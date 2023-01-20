a6bcfd53282361a98f0a8cc01ec10e118bdb637d
Xin Lai <xin.lai-1@ou.edu>
2023-01-19 20:10:49 -0600

    First commit baby!

--- HelloWorld(GITEdition)/src/introduction/Default.java
+++ HelloWorld(GITEdition)/src/introduction/Default.java
@@ -1,7 +1,11 @@
 package introduction;
 
 public class Default {
+<<<<<<< HEAD
 	public static void main(String[] args) {
 		System.out.println("I love version control! My name is Josh!");
 	}
+=======
+
+>>>>>>> 3ea4b27 (First commit baby!)
 }
